1810d4b7244c0499570440fbf2762bc453407d9d
asutosh1993 <asutosh@gurujada.com>
2019-02-05 05:47:42 -0600

    add pagination

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,7 +5,7 @@ class ArticlesController < ApplicationController
     # @article = Article.where(knowledge: 'Learning').first
     # end
             def index 
-                @articles = Article.all
+                @articles = Article.paginate(page: params[:page], per_page: 5)
             end
             def show
                     # render plain: @article.title
