b24dfd4704911fa76bf890cb82db5ad400afef3b
asutosh1993 <asutosh@gurujada.com>
2019-02-04 23:42:14 -0600

    recorrection

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -31,7 +31,7 @@ class ArticlesController < ApplicationController
             end
             def create
                 @article = Article.new(article_params)
-                 @article.user = User.first
+                @article.user = User.first
                 if @article.save
                     #do something
                     flash[:success] ="Article is successfully created"
