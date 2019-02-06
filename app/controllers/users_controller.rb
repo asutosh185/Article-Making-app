fff3886fd2e91f42c396a20b87536c57d494dd55
asutosh1993 <asutosh@gurujada.com>
2019-02-06 01:35:32 -0600

    sign up session update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,9 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if  @user.save
+            session[:user_id] = @user.id
             flash[:success]="Welcome to the alph blog #{@user.username}"
-            redirect_to articles_path
+            redirect_to articles_path(@user)
         else
             render 'new'
         end
